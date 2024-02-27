4658793acea7005b61369acf3e66aea61cfdaf6b
SJaathavan <jaathavansri@gmail.com>
2024-02-27 22:36:17 +0530

    feat(home page):And added routes

---
+++ noknokfront/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import HeroSection from '../components/HeroSection'
+import CardSection from '../components/CardSection'
+
+export default function Home() {
+  return (
+    <div>
+
+<HeroSection/>
+  <CardSection/>
+    </div>
+  )
+}
