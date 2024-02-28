bf388b495799073582ef4a1fcd9f2839bd383518
SJaathavan <jaathavansri@gmail.com>
2024-02-28 22:22:25 +0530

    feat(service route)

--- noknokfront/src/App.jsx
+++ noknokfront/src/App.jsx
@@ -7,6 +7,8 @@ import HomeLayout from './Layouts/HomeLayout'
 import Home from './Pages/Home'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Login from './Pages/Login'
+import ServiceForm from './Pages/SeviceForm'
+import Service from './Pages/Service'
 
 
 function App() {
@@ -21,6 +23,10 @@ function App() {
       {
         path:"/login",element:
   <Login/>
+      },
+      {
+        path:"/serive/add",element:
+  <Service/>
       }
     ]
     }])
