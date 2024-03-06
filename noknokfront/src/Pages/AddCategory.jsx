043063e711f9178a83385e818d965d639bcdf3a5
anujaya2000 <luckyuncle623@gmail.com>
2024-03-06 06:18:38 +0530

    feat(implemneted use admin)

--- noknokfront/src/Pages/AddCategory.jsx
+++ noknokfront/src/Pages/AddCategory.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ServiceForm from './SeviceForm'
 import { CreateCategoryForm } from '../components/CategoryForm'
+import useAdmin from '../hooks/useAdmin'
 
 export default function AddCategory() {
+  useAdmin
   return (
     <div className='w-full flex justify-center items-center mt-16'>
         <CreateCategoryForm/>
--- noknokfront/src/Pages/Profile.jsx
+++ noknokfront/src/Pages/Profile.jsx
@@ -1,15 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ProfileCard2 } from '../components/profile';
 import { GetUser } from '../utils/handleUser';
 import HistoryCard from '../components/Historycard';
 import HotActions from '../components/HotActions';
 import TitleText from '../components/TitleText';
 import useAuth from '../hooks/useAuth';
+import newRequest from '../utils/newRequest';
 
 export default function Profile() {
     const user=GetUser();
     useAuth();
+    const [services, setServices] = useState([]);
+    const [loading, setLoading] = useState(true);
 
+
+  useEffect(() => {
+        const fetchServices = async () => {
+          try {
+            const response =await newRequest.get(`hired-tasks/by-user/${GetUser()._id}`,{
+                params: {
+                    userId: GetUser()._id, 
+                  },
+            });
+            console.log(response)
+            const servicesData = response.data;
+    
+            setServices(servicesData);
+            setLoading(false);
+          } catch (error) {
+            console.error('Error fetching services', error);
+          }
+        };
+    
+        fetchServices();
+      }, []);
+    
   return (
     <div className='mt-14'>
         <TitleText>
@@ -19,7 +44,7 @@ export default function Profile() {
 <ProfileCard2 user={user}/>
 <div>
   
-    <HistoryCard/>
+    <HistoryCard services={services} loading={loading}/>
 </div>
 
 
